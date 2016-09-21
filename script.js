5235660d462c9f92ca6585eaaf7eb52042103960
wallacewylie <wallace.wylie@gmail.com>
2016-09-20 21:28:22 -0500

    Alert code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function myAlert() {
+    alert("Welcome to my world!")
+}
+
 function myFunction() {
     var x = document.getElementById("myText").value;
     document.getElementById("demo").innerHTML = "Hello " + x;
